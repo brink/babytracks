7e354e5aa027e49c4cc06cbd3db63a1363e25fb9
Jonathan Warner <jonwarner@gmail.com>
2011-03-06 15:32:38 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Babytracks::Application.config.secret_token = 'feae4bfbc36d94d801d0406eed6cca38a7c46627478c63c9af9e4893afeb9cc60e1b2f94248c1f3d792a24ef4890cc8d382566a97bc615b6fe2ed158070364cb'
