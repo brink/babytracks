ccdf8911b5a76219a3806f859758a178366b49be
Jonathan Warner <jonwarner@gmail.com>
2011-03-06 22:45:47 -0500

    Added jquery and ajax calls for create page

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,34 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(document).ready(function() {
+  jQuery("#x_feed_link").click(function(event) {
+    event.preventDefault();
+    alert('foo');
+    $.ajax({
+      url: "/events",
+      type: "POST",
+      data: ({'event[event_type]': 'feeding'}),
+      context: document.body,
+      success: function(){
+       location.reload(); 
+      }
+    });
+
+  });
+  jQuery("#x_diaper_link").click(function(event) {
+    event.preventDefault();
+    alert('foo');
+    $.ajax({
+      url: "/events",
+      type: "POST",
+      data: ({'event[event_type]': 'diaper'}),
+      context: document.body,
+      success: function(){
+       location.reload(); 
+      }
+    });
+
+  });
+
+});
