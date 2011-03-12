1462510a3f2457f54d0144abf036ef87414e5095
Jonathan Warner <jonwarner@gmail.com>
2011-03-12 18:49:04 -0500

    Changed links to form, added styles for floating buttons

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,13 +1,10 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
-
 $(document).ready(function() {
-  jQuery("#x_feed_link").click(function(event) {
+  jQuery(".actions form").submit(function(event) {
     event.preventDefault();
     $.ajax({
       url: "/events",
       type: "POST",
-      data: ({'event[event_type]': 'feeding'}),
+      data: jQuery(event.target).serialize(),
       context: document.body,
       success: function(){
        location.reload(); 
@@ -15,18 +12,4 @@ $(document).ready(function() {
     });
 
   });
-  jQuery("#x_diaper_link").click(function(event) {
-    event.preventDefault();
-    $.ajax({
-      url: "/events",
-      type: "POST",
-      data: ({'event[event_type]': 'diaper'}),
-      context: document.body,
-      success: function(){
-       location.reload(); 
-      }
-    });
-
-  });
-
 });
